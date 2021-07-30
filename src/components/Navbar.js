7a262abef8288c35ef93a4e3ce87d6838268564f
Martin Sione <martinsione03@gmail.com>
2021-07-30 19:22:41 -0300

    Add position: sticky; and CartWidget to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,29 @@
 import { NAV_ITEMS } from "../constants/links";
+import CartWidget from "./CartWidget";
 import ThemeSwitcher from "./ThemeSwitcher";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 w-full flex justify-between lg:justify-around border-b dark:border-gray-800 p-3">
-      <a href="/">
-        <img
-          className="flex w-10"
-          src="/images/starbucks-logo.png"
-          alt="logo de coffeeshop"
-        />
-      </a>
+    <nav className="sticky top-0 flex justify-between lg:justify-around border-b bg-white dark:bg-black  dark:border-gray-800 p-3">
+      <div className="flex">
+        <span className="mr-4">
+          <ThemeSwitcher />
+        </span>
+        <a href="/">
+          <img
+            className="w-10"
+            src="/images/starbucks-logo.png"
+            alt="logo de coffeeshop"
+          />
+        </a>
+      </div>
       <div className="text-lg flex items-center">
-        <ul className="flex">
-          {NAV_ITEMS.map((item, key) => (
-            <a href={item.href} key={key}>
-              <li className=" hover:text-gray-700 dark:hover:text-gray-300 mr-4 p-1">
-                {item.name}
-              </li>
-            </a>
-          ))}
-        </ul>
-        <ThemeSwitcher />
+        {NAV_ITEMS.map((item, key) => (
+          <a href={item.href} key={key} className="mr-4 p-1">
+            <p>{item.name}</p>
+          </a>
+        ))}
+        <CartWidget />
       </div>
     </nav>
   );
