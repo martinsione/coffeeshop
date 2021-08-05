945a5e1d0380040129ffadd6b5b78659de9d458a
Martin Sione <martinsione03@gmail.com>
2021-08-05 07:45:54 -0300

    Improve readability for useCounter hook

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,19 +1,12 @@
 import { useState } from "react";
 
 export default function useCounter(initialValue, stock) {
-  const value = stock < 1 ? 0 : parseInt(initialValue) || 1;
+  const value = stock < 1 ? 0 : initialValue;
   const [count, setCount] = useState(value);
 
-  const increment = () => {
-    if (count < parseInt(stock)) {
-      setCount(count + 1);
-    }
-  };
-  const decrement = () => {
-    if (count > 1) {
-      setCount(count - 1);
-    }
-  };
+  const increment = () => count < stock && setCount(count + 1);
+
+  const decrement = () => count > 0 && setCount(count - 1);
 
   return {
     count,
