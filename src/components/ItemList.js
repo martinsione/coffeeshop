5beb36551aa7c2b87848a61fbde564f38b5c5662
Martin Sione <martinsione03@gmail.com>
2021-08-15 16:21:42 -0300

    Add <Link /> to each card

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,16 +1,25 @@
+import { Link } from "react-router-dom";
 import Item from "./Item";
 
 export default function ItemList({ items }) {
+  const handleAdd = () => {};
+
   return (
     <>
       {items.map((item) => (
-        <Item
+        <Link
+          className="max-w-sm w-full md:w-1/2 lg:w-1/3 px-2 py-3"
           key={item.id}
-          title={item.title}
-          description={item.description}
-          price={item.price}
-          pictureUrl={item.pictureUrl}
-        />
+          to={`/item/${item.id}`}
+        >
+          <Item
+            title={item.title}
+            description={item.description}
+            price={item.price}
+            pictureUrl={item.pictureUrl}
+            onAdd={handleAdd}
+          />
+        </Link>
       ))}
     </>
   );
