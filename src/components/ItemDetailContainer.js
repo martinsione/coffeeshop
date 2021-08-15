802e1c6fa594938009600c14ddd1148a00cce25f
Martin Sione <martinsione03@gmail.com>
2021-08-15 16:20:23 -0300

    Add react-router

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,9 +1,25 @@
+import { useParams } from "react-router-dom";
+import Loader from "./Loader";
 import ItemDetail from "./ItemDetail";
+import NotFound from "./NotFound";
 
 export default function ItemDetailContainer({ item }) {
+  const { id } = useParams();
+  let newItem;
+
+  if (item !== "loading") {
+    newItem = item.find((item) => item.id === id);
+  }
+
   return (
     <>
-      <ItemDetail item={item} />
+      {item === "loading" ? (
+        <Loader />
+      ) : newItem ? (
+        <ItemDetail item={newItem} />
+      ) : (
+        <NotFound message={`Item "${id}" was not found.`} />
+      )}
     </>
   );
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,5 +1,10 @@
-export default function ItemListContainer({ children }) {
+import ItemList from "./ItemList";
+import Loader from "./Loader";
+
+export default function ItemListContainer({ itemList }) {
   return (
-    <div className="flex flex-wrap justify-center items-center">{children}</div>
+    <div className="flex flex-wrap justify-center">
+      {itemList === "loading" ? <Loader /> : <ItemList items={itemList} />}
+    </div>
   );
 }
