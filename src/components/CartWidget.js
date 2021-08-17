4020624e4166fac0d05f7b505f14055a288a0d19
Martin Sione <martinsione03@gmail.com>
2021-08-17 17:08:51 -0300

    Changed font-size

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -3,7 +3,7 @@ import { HiShoppingCart } from "react-icons/hi";
 export default function CartWidget() {
   return (
     <>
-      <button className="text-xl">
+      <button className="text-2xl">
         <HiShoppingCart />
       </button>
     </>
