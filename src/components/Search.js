cca903a19af939fb8dabe6809b7a079db327afd4
Martin Sione <martinsione03@gmail.com>
2021-08-17 17:43:27 -0300

    Search can now be done by pressing "Enter"

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,29 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import { IoSearch } from "react-icons/io5";
+import { useHistory } from "react-router-dom";
 
 export default function Search() {
   const [searchValue, setSearchValue] = useState("");
+  const history = useHistory();
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSubmit(e);
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (searchValue.length > 0) {
+      history.push(`/search/${searchValue}`);
+    }
   };
 
   return (
     <form
+      onKeyDown={handleKeyDown}
       onSubmit={handleSubmit}
-      className="wfull flex justify-between items-center border rounded bg-gray-100 dark:bg-gray-900 mx-4"
+      className="w-full max-w-xl flex justify-between items-center border rounded bg-gray-100 dark:bg-gray-900 mx-4"
     >
       <input
         className="w-full bg-gray-100 dark:bg-gray-900 focus:outline-none p-2"
@@ -20,13 +31,9 @@ export default function Search() {
         placeholder="Search..."
         onChange={(e) => setSearchValue(e.target.value)}
       />
-      <Link
-        className="font-bold text-xl p-2"
-        disabled={true}
-        to={`/search/${searchValue}`}
-      >
+      <button className="font-bold text-xl p-2">
         <IoSearch />
-      </Link>
+      </button>
     </form>
   );
 }
