81367a3b2581424b7c269f73c7fde83d88f89e7e
Martin Sione <martinsione03@gmail.com>
2021-09-02 19:07:18 -0300

    Hotfix: Show NotFound when an item does not exist

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -10,7 +10,7 @@ export default function ItemDetailContainer() {
     <>
       {item === "loading" ? (
         <Loader />
-      ) : item ? (
+      ) : item.title ? (
         <ItemDetail item={item} />
       ) : (
         <NotFound message={`Item was not found.`} />
