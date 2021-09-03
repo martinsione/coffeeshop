4758f708ed8a0e52cd7e8b6d7a896b9e57e02091
Martin Sione <martinsione03@gmail.com>
2021-09-03 13:24:14 -0300

    Add buy now button

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,48 +1,42 @@
-import { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import useCounter from "../hooks/useCounter";
 
 export default function ItemCount({ initialValue = 1, stock, onAdd }) {
   const { count, increment, decrement } = useCounter(initialValue, stock);
-  const [added, setAdded] = useState(false);
-
-  const toggleAdded = () => setAdded(added === true ? false : true);
 
   return (
     <div className="flex flex-col min-w-0">
-      {added ? (
+      <div className="flex items-center border rounded text-xl py-2 px-4">
+        <button onClick={decrement} type="button">
+          <AiOutlineMinus onClick={decrement} />
+        </button>
+        <input
+          className="w-full text-center text-base font-semibold dark:bg-black focus:outline-none"
+          readOnly
+          type="number"
+          value={count}
+        />
+        <button onClick={increment} type="button">
+          <AiOutlinePlus />
+        </button>
+      </div>
+      <div className="flex justify-center items-center w-full text-sm font-medium mt-2">
+        <button
+          disabled={stock < 1 || count < 1}
+          className="btn-primary w-full p-1.5 mr-1"
+          onClick={() => onAdd(count)}
+        >
+          Add to cart
+        </button>
         <Link
-          className="flex justify-center rounded text-gray-900 bg-yellow-300 w-full p-2"
+          className="btn-yellow w-full text-center p-1.5 ml-1"
           onClick={() => onAdd(count)}
           to="/cart"
         >
-          Go to cart
+          Buy now
         </Link>
-      ) : (
-        <div className="flex border rounded text-xl py-2 px-4">
-          <button onClick={decrement} type="button">
-            <AiOutlineMinus />
-          </button>
-          <input
-            className="w-full text-center text-base font-semibold dark:bg-black focus:outline-none"
-            type="number"
-            readOnly
-            value={count}
-          />
-          <button onClick={increment} type="button">
-            <AiOutlinePlus />
-          </button>
-        </div>
-      )}
-      <button
-        disabled={stock < 1 || count < 1}
-        className={`border rounded disabled:opacity-50 p-2 mt-1  
-        ${added ? " opacity-80" : "btn-primary"}`}
-        onClick={toggleAdded}
-      >
-        {added === true ? "Cancel" : stock < 1 ? "Sold out" : "Add to cart"}
-      </button>
+      </div>
     </div>
   );
 }
