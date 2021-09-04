5f1f2a795daf16c560275bb7f945ccf8a6652f42
Martin Sione <martinsione03@gmail.com>
2021-09-04 16:49:24 -0300

    Add <select> to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,10 +2,29 @@ import { Link } from "react-router-dom";
 import { useCartContext } from "../context/CartContext";
 
 export default function Cart() {
-  const { items, remove, totalValue, totalItems } = useCartContext();
+  const { items, remove, totalValue, totalItems, updateItemQuantity } =
+    useCartContext();
 
   const handleRemove = (id) => remove(id);
 
+  const quantitySelector = (arr) => {
+    const { quantity, item } = arr;
+    const stockArray = Array.from({ length: item.stock }, (_, i) => i + 1);
+    return (
+      <select
+        className="border rounded font-medium bg-gray-100 dark:bg-gray-900 focus:outline-none p-0.5"
+        value={quantity}
+        onChange={(e) => updateItemQuantity(item.id, Number(e.target.value))}
+      >
+        <option value="0">0 (Delete)</option>
+        {stockArray.map((itemQty) => (
+          <option key={itemQty} value={item.quantity}>
+            {itemQty}
+          </option>
+        ))}
+      </select>
+    );
+  };
   return (
     <>
       {items.length > 0 ? (
@@ -32,7 +51,7 @@ export default function Cart() {
           <div className="lg:w-2/3 lg:pr-16">
             {items.map((arr) => (
               <div
-                className="flex justify-between mb-2 md:mb-4"
+                className="border-b flex justify-between pb-4 mb-4 md:mb-4"
                 key={arr.item.title}
               >
                 <div className="flex">
@@ -43,26 +62,27 @@ export default function Cart() {
                       alt={arr.item.description}
                     />
                   </figure>
-                  <div className="flex flex-col px-3">
-                    <h4 className="font-medium text-lg md:text-xl mb-2 md:mb-4">
-                      {arr.item.title}
-                    </h4>
-                    <span className="font-medium text-xl md:text-2xl mb-2 md:mb-8">
-                      ${arr.item.price}
-                    </span>
-                    <div className="font-medium md:text-lg text-black dark:text-white">
-                      Quantity:
-                      <span className="ml-1">{arr.quantity}</span>
+                  <div>
+                    <div className="flex flex-col px-3">
+                      <h4 className="font-medium text-lg md:text-xl sm:mb-2 md:mb-4">
+                        {arr.item.title}
+                      </h4>
+                      <span className="font-medium text-xl md:text-2xl sm:mb-2 md:mb-8">
+                        ${arr.item.price}
+                      </span>
+                      <div className="flex items-center md:text-lg text-black dark:text-white">
+                        Quantity:
+                        <span className="ml-2">{quantitySelector(arr)}</span>
+                        <button
+                          className="text-sm md:text-base opacity-60 hover:opacity-100 hover:underline ml-2"
+                          onClick={() => handleRemove(arr.item.id)}
+                        >
+                          Delete
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
-
-                <button
-                  className="btn-primary whitespace-nowrap font-medium self-center py-2 px-4"
-                  onClick={() => handleRemove(arr.item.id)}
-                >
-                  Delete
-                </button>
               </div>
             ))}
           </div>
