cc7c0f2bb85f2b64c8f8ac5c7ca8d5d2d5b7c433
Martin Sione <martinsione03@gmail.com>
2021-09-04 18:56:49 -0300

    Add second level navbar again

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,59 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import CartWidget from "../components/CartWidget";
 import { useCartContext } from "../context/CartContext";
 import useTheme from "../hooks/useTheme";
 import Search from "./Search";
 
+const NAV_LINKS = [
+  { name: "Home", href: "/" },
+  { name: "Cart", href: "/cart" },
+  { name: "Orders", href: "/orders" },
+];
+
 export default function Navbar() {
+  const { pathname } = useLocation();
   const { theme, setTheme } = useTheme();
   const { totalItems } = useCartContext();
 
   return (
-    <nav className="sticky top-0 flex justify-between items-center bg-white dark:bg-black py-2 md:mt-8 z-50">
-      <span>
-        <Link className="flex justify-center items-center w-10" to="/">
-          <img
-            className="w-full"
-            src="/images/starbucks-logo.png"
-            alt="Coffeestore logo"
-          />
-        </Link>
-      </span>
-      <Search />
-      <div className="flex">
+    <>
+      <nav className="sticky top-0 flex justify-between items-center bg-white dark:bg-black py-2 md:mt-8 z-50">
+        <span>
+          <Link className="flex justify-center items-center w-10" to="/">
+            <img
+              className="w-full"
+              src="/images/starbucks-logo.png"
+              alt="Coffeestore logo"
+            />
+          </Link>
+        </span>
+        <Search />
+        <div className="flex">
+          <CartWidget totalItems={totalItems()} />
+        </div>
+      </nav>
+      <div className="flex justify-between">
+        <div>
+          {NAV_LINKS.map((navItem) => (
+            <Link
+              className={`text-lg mr-1 p-1 text-black dark:text-white
+          ${pathname === navItem.href ? "font-medium" : "opacity-70"}`}
+              key={navItem.name}
+              to={navItem.href}
+            >
+              {navItem.name}
+            </Link>
+          ))}
+        </div>
         <select
-          className="border rounded bg-gray-100 dark:bg-gray-900 focus:outline-none p-2 mr-4"
+          className="border rounded font-medium bg-gray-100 dark:bg-gray-900 focus:outline-none p-0.5"
           onChange={(e) => setTheme(e.target.value)}
           value={theme}
         >
           <option value="light">Light</option>
           <option value="dark">Dark</option>
         </select>
-        <CartWidget totalItems={totalItems()} />
       </div>
-    </nav>
+    </>
   );
 }
